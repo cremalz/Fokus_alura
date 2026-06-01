962e37401b10ef3af1d48bc4762c4e4d70167d56
cremalz <lorenzocrematrabalho@gmail.com>
2026-06-01 12:53:13 -0300

    ajustes de inicio de crud

--- script-crud.js
+++ script-crud.js
@@ -2,9 +2,21 @@
 
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+const textArea = document.querySelector('.app__form-textarea')
+const tarefas = []
 
 btnAdicionarTarefa.addEventListener('click', () => {
 
     formAdicionarTarefa.classList.toggle('hidden') 
 
+})
+
+
+formAdicionarTarefa.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const tarefa ={
+        descrição: textArea.value
+    }
+    tarefas.push(tarefa)
+    localStorage.setItem('tarefas', tarefas)
 })
