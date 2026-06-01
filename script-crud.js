320a709b8dcef6178ef64e8834ad32ff1ed5140b
cremalz <lorenzocrematrabalho@gmail.com>
2026-06-01 09:44:05 -0300

    adição de funcao para exibir campo que sera preenchido para criação

---
+++ script-crud.js
@@ -0,0 +1,10 @@
+//encontrar o botão adicionar tarefea 
+
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
+const formAdicionarTarefa = document.querySelector('.app__form-add-task')
+
+btnAdicionarTarefa.addEventListener('click', () => {
+
+    formAdicionarTarefa.classList.toggle('hidden') 
+
+})
