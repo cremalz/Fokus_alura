6e9ac9d76c23bc041e2e2106ff864aebadab406e
cremalz <lorenzocrematrabalho@gmail.com>
2026-06-05 17:55:43 -0300

    ajuste de estilo

--- script-crud.js
+++ script-crud.js
@@ -1,65 +1,81 @@
+// encontrar o botão adicionar tarefa
+
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
-const textArea = document.querySelector('.app__form-textarea')
+const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
-const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] 
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+function atualizarTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
-    li.classList.add('app__section-task-list-item') 
+    li.classList.add('app__section-task-list-item')
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+                fill="#01080E"></path>
         </svg>
     `
     const paragrafo = document.createElement('p')
-    paragrafo.textContent = tarefa.descricao 
+    paragrafo.textContent = tarefa.descricao
     paragrafo.classList.add('app__section-task-list-item-description')
 
-    const button = document.createElement('button')
-    button.classList.add('app_button-edit')
-    const imagemBotao = document.createElement('img')
+    const botao = document.createElement('button')
+    botao.classList.add('app_button-edit')
 
-    button.onclick = () => {
-        const novaDescricao = prompt("Qual é o nome da tarefa?", tarefa.descricao)
-        if (novaDescricao) {
-            tarefa.descricao = novaDescricao
+    botao.onclick = () => {
+        // debugger
+        const novaDescricao = prompt("Qual é o novo nome da tarefa?")
+        // console.log('Nova descrição da tarefa: ', novaDescricao)
+        if (novaDescricao) {            
             paragrafo.textContent = novaDescricao
-            localStorage.setItem('tarefas', JSON.stringify(tarefas))
+            tarefa.descricao = novaDescricao
+            atualizarTarefas()
         }
     }
 
-    imagemBotao.setAttribute('src', './imagens/edit.png')
-    button.appendChild(imagemBotao)
-    li.appendChild(svg)
-    li.appendChild(paragrafo)
-    li.appendChild(button)
+    const imagemBotao = document.createElement('img')
+    imagemBotao.setAttribute('src', '/imagens/edit.png')
+    botao.append(imagemBotao)
 
-    return li 
+    li.append(svg)
+    li.append(paragrafo)
+    li.append(botao)
+
+li.onclick = () => {
+    paragrafoDescricaoTarefa.textContent = tarefa.descricao
 }
+
+    return li
+}   
 
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden')
 })
 
-formAdicionarTarefa.addEventListener('submit', (event) => {
-    event.preventDefault()
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
     const tarefa = {
-        descricao: textArea.value
+        descricao: textarea.value
     }
     tarefas.push(tarefa)
     const elementoTarefa = criarElementoTarefa(tarefa)
-    ulTarefas.appendChild(elementoTarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
-    textArea.value = ''
+    ulTarefas.append(elementoTarefa)
+    atualizarTarefas()
+    textarea.value = ''
     formAdicionarTarefa.classList.add('hidden')
 })
 
 tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa)
     ulTarefas.append(elementoTarefa)
-})
+});
